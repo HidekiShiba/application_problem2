0adcf9666b6791bf138ffe9aba37929e8755b0e2
HidekiShiba <hshiba033@gmail.com>
2020-12-16 21:55:29 +0900

    [Finish] バグ修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,26 +1,33 @@
 class BooksController < ApplicationController
+  def index
+    @books = Book.all
+    @book = Book.new
+    @book.user_id = current_user.id
+  end
 
   def show
     @book = Book.find(params[:id])
-  end
-
-  def index
-    @books = Book.all
+    @book_new = Book.new
   end
 
   def create
+    @books = Book.all
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
-      @books = Book.all
       render 'index'
     end
   end
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user == current_user
+      render :edit
+    else
+      redirect_to books_path
+    end
   end
 
 
@@ -34,16 +41,16 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,33 @@
 class UsersController < ApplicationController
   before_action :ensure_correct_user, only: [:update]
 
+  def index
+    @users = User.all
+    @user = current_user
+    @book = Book.new
+  end
+
   def show
     @user = User.find(params[:id])
     @books = @user.books
     @book = Book.new
   end
 
-  def index
-    @users = User.all
-    @book = Book.new
-  end
-
   def edit
     @user = User.find(params[:id])
+    if @user == current_user
+      render :edit
+    else
+      redirect_to user_path(current_user)
+    end
   end
 
   def update
+    @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+      redirect_to user_path(@user), notice: "You have updated user successfully."
     else
-      render "show"
+      render :edit
     end
   end
 
